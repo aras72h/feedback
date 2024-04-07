1664a8637decb623c64f39d728b43f479774ea1b
Arash A <43763536+aras72h@users.noreply.github.com>
2024-04-07 15:22:32 +0330

    Import App.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 /* This file is the entry point to React apps */
 import React from 'react'
 import ReactDOM from 'react-dom'
+import App from './App'
 
-ReactDOM.render(<h1>hello, world</h1>, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById('root'))
