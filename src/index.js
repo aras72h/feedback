27930cb2760dca82d57a442a4d09adabe47e4d07
Arash A <43763536+aras72h@users.noreply.github.com>
2024-04-07 14:56:33 +0330

    Hello world using render method

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 /* This file is the entry point to React apps */
 import React from 'react'
 import ReactDOM from 'react-dom'
+
+ReactDOM.render(<h1>hello, world</h1>, document.getElementById('root'))
