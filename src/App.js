96a0e7f5a5384263c2fa74fa87344804834f05f0
Arash A <43763536+aras72h@users.noreply.github.com>
2024-04-09 14:22:53 +0330

    Import Header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import Header from './components/Header'
+
 /* The main front-end app. Pass this file to index.html > ReactDOM.rendere() method */
 function App() {
     return (
         // Wrap all of elements with one parent element like div
         // Use an empty fragment if you don't want a parent element
         <>
+            <Header />
             <h1>hello, world</h1>
             {/* Use className instead of class attribute */}
             <p className="some-class-name">this is a react app</p>
